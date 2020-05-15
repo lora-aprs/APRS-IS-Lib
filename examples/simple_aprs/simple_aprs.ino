cf89cc85ed45d431ac39b8693750fac0d3b2fb8e
Peter Buchegger <peter@pbuchegger.at>
2020-05-15 04:13:22 +0200

    next fix

--- examples/simple_aprs/simple_aprs.ino
+++ examples/simple_aprs/simple_aprs.ino
@@ -1,9 +1,13 @@
-#include <WiFiMulti.h>
+
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#else
+#include <WiFi.h>
+#endif
 #include <APRS-IS.h>
 
 #include "settings.h"
 
-WiFiMulti WiFiMulti;
 APRS_IS aprs_is(USER, PASS, TOOL, VERS);
 
 void setup()
@@ -11,10 +15,11 @@ void setup()
 	Serial.begin(115200);
 	Serial.println("simple APRS-IS test");
 
-	WiFiMulti.addAP(WIFI_NAME, WIFI_KEY);
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(WIFI_NAME, WIFI_KEY);
 	Serial.print("Waiting for WiFi");
 
-	while(WiFiMulti.run() != WL_CONNECTED)
+	while(WiFi.status() != WL_CONNECTED)
 	{
 		Serial.print(".");
 		delay(500);
@@ -30,12 +35,6 @@ void setup()
 
 void loop()
 {
-	if(WiFiMulti.run() != WL_CONNECTED)
-	{
-		Serial.println("WiFi not connected!");
-		delay(1000);
-		return;
-	}
 	if(!aprs_is.connected())
 	{
 		Serial.print("connecting to server: ");
